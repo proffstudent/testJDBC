9e3819e1df649b50d3f7b338ee5732cfdc3969b4
Mak Ogorodov <proffstudent@ya.ru>
2017-02-16 15:54:25 +0300

    Готово для класса Students

--- src/su/doma_dachi/inno/work/TestJDBC.java
+++ src/su/doma_dachi/inno/work/TestJDBC.java
@@ -23,40 +23,5 @@ public class TestJDBC {
         student.addStudent(7, "vasya", 20);
         student.delStudent(7);
         student.delStudent("petya");
-
     }
-    public static void Select(String url, String name, String password){
-        try(Connection connection = DriverManager.getConnection(url, name, password) ){
-            Statement query = connection.createStatement();
-            ResultSet resultSet = query.executeQuery("Select * from public.users");
-            while (resultSet.next()){
-                System.out.println("Номер в выборке #" + resultSet.getRow()
-                        + "\t Номер в базе #" + resultSet.getInt("id")
-                        + "\t" + resultSet.getString("username"));
-            }
-
-        }catch (SQLException e){
-            e.printStackTrace();
-        }finally {
-            //connection.close();
-        }
-    }
-    public static void preparedStatement(String url, String name, String password){
-        try(Connection connection = DriverManager.getConnection(url, name, password) ){
-            String sql = "INSERT INTO public.users (id, email, username, password) values(6,'sfdds','sdfsd','sfs')";
-            PreparedStatement query = connection.prepareStatement(sql);
-            //query.setString(0, "adfs");
-           // query.setString(1, 6);
-//            query.setString(2, "adfs");
-//            query.setString(3, "adfs");
-//            query.setString(4, "adfs");
-            System.out.println(query.executeUpdate());
-
-        }catch (SQLException e){
-            e.printStackTrace();
-        }finally {
-            //connection.close();
-        }
-    }
-
 }
